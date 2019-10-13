8d27ad394256ef68aa90f6b3882536898e97b6d0
Craig Smith <vxdhost@gmail.com>
2019-10-13 20:49:04 +0000

    Apply fixes from StyleCI

--- src/Events/Deleted.php
+++ src/Events/Deleted.php
@@ -23,7 +23,7 @@ class Deleted
      */
     public function __construct($record, $request)
     {
-		$this->record = $record;
-		$this->request = $request;
+        $this->record = $record;
+        $this->request = $request;
     }
 }
