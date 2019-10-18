502f33b97b6aec883583cc29984d847c652a5727
Craig Smith <952595+phpsa@users.noreply.github.com>
2019-10-18 15:22:18 +1300

    Request should have a constructor for instances with models

--- src/Repository/BaseRepository.php
+++ src/Repository/BaseRepository.php
@@ -74,6 +74,13 @@ class BaseRepository
      * @var array
      */
     protected $scopes = [];
+    
+    public function __construct()
+	{
+		if(method_exists($this, 'model')){
+			$this->makeModel($this->model())
+		}
+	}
 
     public static function withModel(string $model)
     {
