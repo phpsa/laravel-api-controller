887ee528ef4ee4965d002439ddc1a2c8b73e4a8d
Craig Smith <craig.smith@customd.com>
2019-11-29 09:51:16 +1300

    csfixer run on code

--- src/Contracts/Parser.php
+++ src/Contracts/Parser.php
@@ -2,8 +2,8 @@
 
 namespace Phpsa\LaravelApiController\Contracts;
 
-use Phpsa\LaravelApiController\UriParser;
 use Phpsa\LaravelApiController\Exceptions\UnknownColumnException;
+use Phpsa\LaravelApiController\UriParser;
 
 trait Parser
 {
@@ -51,7 +51,7 @@ trait Parser
 
             if (! empty($fields)) {
                 $fields[] = $sub->getKeyName();
-                $withs[$idx] = $with . ':' . implode(',', array_unique($fields));
+                $withs[$idx] = $with.':'.implode(',', array_unique($fields));
             }
         }
 
@@ -181,12 +181,12 @@ trait Parser
     {
         $fields = $this->request->has('fields') && ! empty($this->request->input('fields')) ? explode(',', $this->request->input('fields')) : $this->defaultFields;
         foreach ($fields as $key => $field) {
-            if (strpos($field, $include . '.') === false) {
+            if (strpos($field, $include.'.') === false) {
                 unset($fields[$key]);
 
                 continue;
             }
-            $fields[$key] = str_replace($include . '.', '', $field);
+            $fields[$key] = str_replace($include.'.', '', $field);
         }
 
         return $fields;
