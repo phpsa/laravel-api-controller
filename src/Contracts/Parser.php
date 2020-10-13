c374cc7befa101df26daeb31417895fa299e63c0
Craig Smith <952595+phpsa@users.noreply.github.com>
2020-10-14 10:38:07 +1300

    includes fields patched to not try select unknown fields

--- src/Contracts/Parser.php
+++ src/Contracts/Parser.php
@@ -281,12 +281,19 @@ trait Parser
     {
         /** @scrutinizer ignore-call */
         $fields = Helpers::filterFieldsFromRequest($this->request, $this->/** @scrutinizer ignore-call */ getDefaultFields());
+
+        $relation = self::$model->{$include}();
+         $type = $relation->getRelated();
+         $tableColumns = $this->getTableColumns($type);
+
         foreach ($fields as $key => $field) {
-            if (strpos($field, Helpers::snake($include).'.') === false) {
+            $parts = explode(".", $field);
+            if (strpos($field, Helpers::snake($include).'.') === false || !in_array(end($parts), $tableColumns)) {
                 unset($fields[$key]);
 
                 continue;
             }
+
             $fields[$key] = str_replace(Helpers::snake($include).'.', '', $field);
         }
 
