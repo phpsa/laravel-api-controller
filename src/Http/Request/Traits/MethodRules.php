aca6307c61426ea9509b5780abbe2088c9691f5b
Craig Smith <craig.smith@customd.com>
2021-02-02 16:52:37 +1300

    feature: New trait for validation rules based on request action

---
+++ src/Http/Request/Traits/MethodRules.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Phpsa\LaravelApiController\Http\Requests\Traits;
+
+trait MethodRules
+{
+
+    public function rules()
+    {
+        if (! $this->route()) {
+            return [];
+        }
+
+        $methodRules = $this->route()->getActionMethod() . 'Rules';
+
+        return method_exists($this, $methodRules)
+        ? array_merge($this->commonRules(), $this->$methodRules())
+        : $this->commonRules();
+    }
+}
