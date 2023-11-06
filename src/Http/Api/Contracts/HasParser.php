9050b9ab8a34df87814944303e0193d7df835dcd
Craig Smith <craig.smith@customd.com>
2023-11-07 11:45:44 +1300

    fix: nested relations with camel cased methods

--- src/Http/Api/Contracts/HasParser.php
+++ src/Http/Api/Contracts/HasParser.php
@@ -3,11 +3,12 @@
 namespace Phpsa\LaravelApiController\Http\Api\Contracts;
 
 use RuntimeException;
+use Illuminate\Support\Str;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Phpsa\LaravelApiController\Helpers;
+use Illuminate\Database\Eloquent\Builder;
 use Phpsa\LaravelApiController\UriParser;
 use Phpsa\LaravelApiController\Traits\FiltersBuilder;
 
@@ -64,9 +65,9 @@ trait HasParser
 
         if (is_array($parent)) {
             $key = key($parent);
-            $param = strtolower(class_basename(reset($parent)));
+            $param = Str::snake(class_basename(reset($parent)));
         } else {
-            $key = strtolower(class_basename($parent));
+            $key = Str::snake(class_basename($parent));
             $param = $key;
         }
 
@@ -74,21 +75,24 @@ trait HasParser
 
         $child = resolve($this->model());
 
-        if (! $routeRelation instanceof Model) {
-            $bindingField = $this->request->route()->bindingFieldFor($param) ?? $child->{$key}()->getRelated()->getKeyName();
-            $routeRelation = $child->{$key}()->getRelated()->where($bindingField, $routeRelation)->firstOrFail();
+        $relationName = Str::camel($param);
+        $relation = $child->{$relationName}();
+
+        if (!$routeRelation instanceof Model) {
+            $bindingField = $this->request->route()->bindingFieldFor($param) ?? $relation->getRelated()->getKeyName();
+            $routeRelation = $relation->getRelated()->where($bindingField, $routeRelation)->firstOrFail();
         }
 
         $parentPolicy = Gate::getPolicyFor($routeRelation);
 
-        if(!is_null($parentPolicy)){
+        if (!is_null($parentPolicy)) {
             $this->authorize('view', $routeRelation);
         }
 
-          $filter = match (class_basename(get_class($child->{$key}()))) {
-            'HasOne' => $child->{$key}()->getLocalKeyName(),
-            'BelongsToMany' => $key . '.' . $child->{$key}()->getRelatedKeyName(),
-            default => $child->{$key}()->getForeignKeyName(),
+        $filter = match (class_basename(get_class($relation))) {
+            'HasOne' => $relation->getLocalKeyName(),
+            'BelongsToMany' => $key . '.' . $relation->getRelatedKeyName(),
+            default => $relation->getForeignKeyName(),
         };
 
 
@@ -122,7 +126,7 @@ trait HasParser
         foreach ($sorts as $sort) {
             $sortP = explode(' ', $sort);
             $sortF = $sortP[0];
-            $sortD = ! empty($sortP[1]) && strtolower($sortP[1]) === 'desc' ? 'desc' : 'asc';
+            $sortD = !empty($sortP[1]) && strtolower($sortP[1]) === 'desc' ? 'desc' : 'asc';
 
             if (strpos($sortF, '.') > 0) {
                 $withSorts[$sortF] = $sortD;
@@ -131,7 +135,7 @@ trait HasParser
             /** @scrutinizer ignore-call */
             $tableColumns = $this->getTableColumns();
 
-            if (empty($sortF) || ! in_array($sortF, $tableColumns)) {
+            if (empty($sortF) || !in_array($sortF, $tableColumns)) {
                 continue;
             }
 
@@ -173,8 +177,9 @@ trait HasParser
                     ->whereColumn(
                         $relation->getRelated()->qualifyColumn($foreignKey),
                         $this->getBuilder()->qualifyColumn($localKey),
-                    )->orderBy($key)->limit(1), $sortD);
-
+                    )->orderBy($key)->limit(1),
+                $sortD
+            );
         }
     }
 
@@ -188,7 +193,7 @@ trait HasParser
         $sortField = config('laravel-api-controller.parameters.sort');
         $sort = $this->request->has($sortField) ? $this->request->input($sortField) : $this->defaultSort;
 
-        if (! $sort) {
+        if (!$sort) {
             return [];
         }
 
@@ -215,12 +220,16 @@ trait HasParser
 
         foreach ($where as $whr) {
             if (strpos($whr['key'], '.') > 0) {
-                $this->/** @scrutinizer ignore-call */setWhereHasClause($whr);
+                $this->
+                /** @scrutinizer ignore-call */
+                setWhereHasClause($whr);
                 continue;
-            } elseif (! in_array($whr['key'], $tableColumns)) {
+            } elseif (!in_array($whr['key'], $tableColumns)) {
                 continue;
             }
-            $this->/** @scrutinizer ignore-call */setQueryBuilderWhereStatement($this->getBuilder(), $table . '.' . $whr['key'], $whr);
+            $this->
+            /** @scrutinizer ignore-call */
+            setQueryBuilderWhereStatement($this->getBuilder(), $table . '.' . $whr['key'], $whr);
         }
     }
 
@@ -228,7 +237,7 @@ trait HasParser
     {
         $this->parseFiltersArray($this->request->input('filters', []))
             ->each(
-                fn($filter, $column) => collect($filter)->each(fn($value, $comparison) => $this->buildQuery($column, $comparison, $value, $this->getBuilder()))
+                fn ($filter, $column) => collect($filter)->each(fn ($value, $comparison) => $this->buildQuery($column, $comparison, $value, $this->getBuilder()))
             );
     }
 
@@ -240,8 +249,8 @@ trait HasParser
     protected function parseFieldParams(): array
     {
         $default = $this->getDefaultFields();
-        if($default !== ['*']){
-            $default = array_merge( $default, $this->getAlwaysSelectFields());
+        if ($default !== ['*']) {
+            $default = array_merge($default, $this->getAlwaysSelectFields());
         }
 
         $fields = Helpers::filterFieldsFromRequest(
@@ -270,7 +279,7 @@ trait HasParser
         $limitField = config('laravel-api-controller.parameters.limit') ?? 'limit';
         $limit = $this->request->has($limitField) ? intval($this->request->input($limitField)) : $this->getDefaultLimit();
 
-        if ($this->maximumLimit && ($limit > $this->maximumLimit || ! $limit)) {
+        if ($this->maximumLimit && ($limit > $this->maximumLimit || !$limit)) {
             $limit = $this->maximumLimit;
         }
 
