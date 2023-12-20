f7a3ffc02655f049522145f22c53cdadea55fae3
Craig Smith <952595+phpsa@users.noreply.github.com>
2023-12-20 14:16:30 +1300

    updated to new syntax

--- src/Http/Resources/Contracts/CaseFormat.php
+++ src/Http/Resources/Contracts/CaseFormat.php
@@ -2,22 +2,21 @@
 
 namespace Phpsa\LaravelApiController\Http\Resources\Contracts;
 
+use Illuminate\Http\Request;
 use Phpsa\LaravelApiController\Helpers;
 
 trait CaseFormat
 {
-    protected function caseFormat($request, $data)
+    protected function caseFormat(Request $request, array $data): array
     {
-        switch (strtolower((string)$request->header('X-Accept-Case-Type'))) {
-            case 'camel':
-            case 'camel-case':
-                return Helpers::camelCaseArrayKeys($data);
-
-            case 'snake':
-            case 'snake-case':
-                return Helpers::snakeCaseArrayKeys($data);
+        $case = $request->header('X-Accept-Case-Type');
+        if (!is_string($case)) {
+            return $data;
         }
-
-        return $data;
+        return match (str($case)->lower()) {
+            'camel', 'camel-case' => Helpers::camelCaseArrayKeys($data),
+            'snake', 'snake-case' => Helpers::snakeCaseArrayKeys($data),
+            default => $data,
+        };
     }
 }
